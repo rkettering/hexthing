1f86ff19e6164f6f1fa61c6361ecf156b6198830
Richard Kettering <kettering.richard@gmail.com>
2013-02-22 22:46:20 -0600

    Display refactoring.

--- scripts/code.js
+++ scripts/code.js
@@ -48,10 +48,6 @@ function init_display(){
 }
 
 
-var tileHeight = 48;
-var tileVertOffset = (tileHeight - tileHeight/3);
-var tileHeightRepeat = (tileVertOffset*2);
-var tileWidth = 64;
 
 function handle_mouse_click(e,canvas){
 	
@@ -92,51 +88,44 @@ function tile_at_coords(x, y){
 
 function row_given(x,y){
 	//boost the row by one if we're on a peak jutting up from the row below
-	var rise = tileHeight/3;
-	var run = tileWidth/2;
+	var rise = gamedata.tileHeight/3;
+	var run = gamedata.tileWidth/2;
 	function slope(){ return rise/run};
-	function flipped(y){ return y % tileHeightRepeat >= tileVertOffset };
-	function is_slope_rising(x){  if(flipped(y))	return (x % tileWidth) <= tileWidth/2;
-									else			return (x % tileWidth) > tileWidth/2;  };
+	function flipped(y){ return y % gamedata.tileHeightRepeat >= gamedata.tileVertOffset };
+	function is_slope_rising(x){  if(flipped(y))	return (x % gamedata.tileWidth) <= gamedata.tileWidth/2;
+									else			return (x % gamedata.tileWidth) > gamedata.tileWidth/2;  };
 	
 
 
 	function y_offset(x){ 
 		if( is_slope_rising(x) ){
-			return (x % (tileWidth/2)) * slope();
+			return (x % (gamedata.tileWidth/2)) * slope();
 		} else {
-			return (tileWidth/2)*slope() - ((x % (tileWidth/2)) * slope());
+			return (gamedata.tileWidth/2)*slope() - ((x % (gamedata.tileWidth/2)) * slope());
 		}
 	};
 
 	function notch() {
-		if( (y % tileVertOffset) > y_offset(x)) return 1;
+		if( (y % gamedata.tileVertOffset) > y_offset(x)) return 1;
 		else return 0;
 	};
 	
-	function rectangular_row_for_y(){ return misc.truncate( y / tileVertOffset) };
+	function rectangular_row_for_y(){ return misc.truncate( y / gamedata.tileVertOffset) };
 	return  notch() + rectangular_row_for_y() - 1;
 }
 
 function column_given_x_and_row(x,row){
-	function alt_x(){ return row%2 == 1 ? x - (tileWidth/2) : x; }; 
-	return misc.truncate( alt_x() / tileWidth);
+	function alt_x(){ return row%2 == 1 ? x - (gamedata.tileWidth/2) : x; }; 
+	return misc.truncate( alt_x() / gamedata.tileWidth);
 }
 
 
 function display_game(data){
 
-	display_tiles(data);
+	gamedata.display_tiles(data);
 	display_sidebar(data);
 
-	function display_tiles(data){
-		$.each(data.tiles, function(outerindex,outervalue) {
-		
-		
-			$.each(outervalue, function(index,value) { ctx.drawImage(util.images_cache[( value.imageName() )],index*tileWidth +(tileWidth/2)*((outerindex)%2) ,outerindex* tileVertOffset); /*console.log(index);*/  });
-			
-		 });
-	}
+
 
 	function display_sidebar(data){
 		clear_canvas(side_ctx);
--- scripts/gameplay.js
+++ scripts/gameplay.js
@@ -1,6 +1,12 @@
 var gamedata = null;
 gamedata = new Object();
 
+gamedata.tileHeight = 48;
+gamedata.tileVertOffset = (gamedata.tileHeight - gamedata.tileHeight/3);
+gamedata.tileHeightRepeat = (gamedata.tileVertOffset*2);
+gamedata.tileWidth = 64;
+
+
 gamedata.load_graphics = function () {
 	util.require_image_before_next_draw('images/green.png');
 	util.require_image_before_next_draw('images/teal.png');
@@ -20,6 +26,16 @@ gamedata.act_on_tile = function(x, y) {
 	gamedata.tiles[x][y]._tileType = 'indigo.png';
 	gamedata.add_moves( -1 );
 }
+
+gamedata.display_tiles = function (data){
+		$.each(data.tiles, function(outerindex,outervalue) {
+		
+		
+			$.each(outervalue, function(index,value) { ctx.drawImage(util.images_cache[( value.imageName() )],index*gamedata.tileWidth +(gamedata.tileWidth/2)*((outerindex)%2) ,outerindex* gamedata.tileVertOffset); /*console.log(index);*/  });
+			
+});
+}
+
 
 gamedata.end_turn = function() {
 	gamedata.moves = 2;
